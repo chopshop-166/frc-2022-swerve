84d332116739742d48d5feb8d9fc1aef4c076f8a
Matt Soucy <git@msoucy.me>
2021-08-21 11:23:20 -0400

    Make it build!

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,17 +6,9 @@ package frc.robot;
 
 import com.chopshop166.chopshoplib.commands.CommandRobot;
 
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.maps.RobotMap;
-import frc.robot.subsystems.Drive;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Spindexer;
-import frc.robot.subsystems.Turret;
 import io.github.oblarg.oblog.Logger;
 
 /**
