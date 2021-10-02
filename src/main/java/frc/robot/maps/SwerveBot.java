e068a4e60a706fceb25c413599cd87ed3a679d7a
Ben <bot190@gmail.com>
2021-10-01 23:11:00 -0400

    Take advantage of new PIDSparkMax constructor
    Small clean-up to make Drive Map consistent.

--- src/main/java/frc/robot/maps/SwerveBot.java
+++ src/main/java/frc/robot/maps/SwerveBot.java
@@ -26,7 +26,7 @@ public class SwerveBot extends RobotMap {
 
         // All Distances are in Meters
         // Front Left Module
-        final PIDSparkMax frontLeftController = new PIDSparkMax(new CANSparkMax(1, MotorType.kBrushless));
+        final PIDSparkMax frontLeftController = new PIDSparkMax(1, MotorType.kBrushless);
         final CANCoder encoderFL = new CANCoder(1);
         encoderFL.configMagnetOffset(-36.0078125);
         encoderFL.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
@@ -36,7 +36,7 @@ public class SwerveBot extends RobotMap {
                 new PIDSparkMax(2, MotorType.kBrushless), frontLeftController);
 
         // Front Right Module
-        final PIDSparkMax frontRightController = new PIDSparkMax(new CANSparkMax(3, MotorType.kBrushless));
+        final PIDSparkMax frontRightController = new PIDSparkMax(3, MotorType.kBrushless);
         final CANCoder encoderFR = new CANCoder(2);
         encoderFR.configMagnetOffset(-293.02734375000006);
         encoderFR.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
@@ -46,7 +46,7 @@ public class SwerveBot extends RobotMap {
                 new PIDSparkMax(4, MotorType.kBrushless), frontRightController);
 
         // Rear Left Module
-        final PIDSparkMax rearLeftController = new PIDSparkMax(new CANSparkMax(5, MotorType.kBrushless));
+        final PIDSparkMax rearLeftController = new PIDSparkMax(5, MotorType.kBrushless);
         final CANCoder encoderRL = new CANCoder(3);
         encoderRL.configMagnetOffset(-102.6562);
         encoderRL.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
@@ -56,7 +56,7 @@ public class SwerveBot extends RobotMap {
                 new PIDSparkMax(6, MotorType.kBrushless), rearLeftController);
 
         // Rear Right Module
-        final PIDSparkMax rearRightController = new PIDSparkMax(new CANSparkMax(7, MotorType.kBrushless));
+        final PIDSparkMax rearRightController = new PIDSparkMax(7, MotorType.kBrushless);
         final CANCoder encoderRR = new CANCoder(4);
         encoderRR.configMagnetOffset(-269.121);
         encoderRR.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
